426fb9e5fea3ee5c11270da434aa6e3885231ae4
MrKrot792 <vkorotanec@gmail.com>
2025-02-05 21:14:06 +0100

    added few things

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -15,90 +15,88 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "css", "html" },
-				auto_install = true,
+				ensure_installed = { "lua_ls", "clangd" },
+				automatic_installation = true, -- Автоматическая установка LSP
 			})
 		end,
 	},
 	{
-
 		"neovim/nvim-lspconfig",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp", -- Явно указываем зависимость
+		},
 		config = function()
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
+			local lspconfig = require("lspconfig")
 
+			-- Общие настройки для всех LSP
+			local common_capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local common_on_attach = function(client, bufnr)
+				-- Общие keymaps и autocmd
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr })
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+				vim.keymap.set("n", "<leader>ff", function()
+					vim.lsp.buf.format({ async = true })
+				end, { buffer = bufnr })
+			end
+
+			-- Автоматическая настройка LSP серверов из Mason
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = common_capabilities,
+						on_attach = common_on_attach,
+					})
+				end,
+				-- Специфичные настройки для отдельных LSP
+				["lua_ls"] = function()
+					lspconfig.lua_ls.setup({
+						capabilities = common_capabilities,
+						on_attach = common_on_attach,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
+				["clangd"] = function()
+					lspconfig.clangd.setup({
+						cmd = { "clangd", "--fallback-style=Microsoft" },
+						capabilities = common_capabilities,
+						on_attach = common_on_attach,
+					})
+				end,
+			})
+
+			-- Настройка cmp
 			cmp.setup({
 				formatting = {
 					format = lspkind.cmp_format({
-						mode = "symbol", -- show only symbol annotations
-						maxwidth = {
-							-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-							-- can also be a function to dynamically calculate max width such as
-							-- menu = function() return math.floor(0.45 * vim.o.columns) end,
-							menu = 50, -- leading text (labelDetails)
-							abbr = 50, -- actual suggestion item
-						},
-						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-
-						-- The function below will be called before any actual modifications from lspkind
-						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-						before = function(entry, vim_item)
-							-- ...
-							return vim_item
-						end,
+						mode = "symbol",
+						maxwidth = 50,
+						ellipsis_char = "...",
 					}),
 				},
-
 				sources = {
 					{ name = "nvim_lsp" },
 					{ name = "buffer" },
 					{ name = "path" },
 				},
-
 				mapping = {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
-							cmp.select_next_item() -- Просто перейти к следующему элементу
+							cmp.select_next_item()
 						else
-							fallback() -- Вставить табуляцию, если автокомплит не активен
+							fallback()
 						end
 					end, { "i", "s" }),
 				},
 			})
-
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-			local lspconfig = require("lspconfig")
-
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end,
-			})
-
-			lspconfig.clangd.setup({
-				cmd = { "clangd", "--fallback-style=Microsoft" },
-				capabilities = capabilities,
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end,
-			})
-
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
 }
